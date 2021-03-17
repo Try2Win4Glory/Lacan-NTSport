c59bd6f9062f4d3ff8fe4e5c3e10f71efefaf852
Try2Win4Code <try2win4glory@gmail.com>
2021-03-17 23:05:39 +0000

    Changed it to Race as Standard and added better design for verify.py

--- commands/nitrotype/verify.py
+++ commands/nitrotype/verify.py
@@ -21,7 +21,7 @@ class Command(commands.Cog):
             async with session.get(url) as response:
                 return await response.text()
     @commands.command()
-    async def verify(self, ctx, type="car"):
+    async def verify(self, ctx, type="race"):
         #return await ctx.send('This command is currently under maintenance. The developers will try to get it up again as soon as possible. In the meantime feel free to use `n.help` to get the other commands. Thank you for your understanding!')
         if type == 'car':
             return await verify(ctx)
@@ -32,9 +32,9 @@ class Command(commands.Cog):
             for elem in dbdata['registered']:
                 if elem['userID'] == str(ctx.author.id):
                     if elem['verified'] == 'false':
-                        embed = Embed('Please Join This Race!', 'Join This Race To Verify This Account Is Yours')
-                        embed.field('Instructions', 'Once you join this race, the race leader will leave and you just have to type `n.verify` again to verify. If this does not work, please try doing the verification again.')
-                        embed.field('Link', '[:link:](https://www.nitrotype.com/race/lacanverification)')
+                        embed = Embed(':race_car:  Please Join This Race!', 'Join This Race To Verify This Account Is Yours')
+                        embed.field('__Instructions__', 'Once you join this race, the race leader will leave and you just have to type `n.verify race` again to verify. If this does not work, please try doing the verification again.')
+                        embed.field('__Link__', '[:link:](https://www.nitrotype.com/race/lacanverification)')
                         await embed.send(ctx)
                         elem['verifyCar'] = None
                         elem['verified'] = 'in progress'
@@ -57,17 +57,18 @@ class Command(commands.Cog):
                             await embed.send(ctx)
                             break
                         else:
-                            embed = Embed('Error!', 'Oops it does not seem like you are verified!')
-                            embed.field('Instructions', 'Please try to do `n.verify` again and join the race!')
+                            embed = Embed('Error!', 'Oops it does not seem like you are verified!', 'warning')
+                            embed.field('__Instructions__', 'Please try to do `n.verify race` again and join the race!')
+                            embed.field('__Link__', '[:link:](https://www.nitrotype.com/race/lacanverification)')
                             await embed.send(ctx)
                             async with aiohttp.ClientSession() as s:
                                 await self.fetch(s,'https://nebuliteforgold-2.adl212.repl.co')
                             break
                     if elem['verified'] == 'true':
-                        embed = Embed('bru', 'You are already verified :rofl:')
+                        embed = Embed('Error!', 'You are already verified :rofl:', 'joy')
                         return await embed.send(ctx)
             else:
-                embed = Embed('bru', 'You have not registered yet. Do `n.register <username>`')
+                embed = Embed('Error!', 'You have not registered yet. Do `n.register <username>`', 'warning')
                 await embed.send(ctx)
 def setup(client):
     client.add_cog(Command(client))
