b53924577bb93545497083c7d47c194081eea22d
Try2Win4Glory <65898600+Try2Win4Glory@users.noreply.github.com>
2021-03-26 08:50:19 +0100

    New design for servernum.py
    Added it to be more mobile friendly and much better looking

--- commands/useful/servernum.py
+++ commands/useful/servernum.py
@@ -30,12 +30,21 @@ class Command(commands.Cog):
 
         guilds = len(self.client.guilds)
         divided_users = totalusers/guilds
+
+        '''embed=Embed(':1234:  Server Number', f'Check in how many servers I\'m used in!')
+        embed.field('**__Guilds:__**', f'**`{len(self.client.guilds)}`**')
+        embed.field('**__Total users:__**', f'**`{comma_users}`**')
+        embed.field('**__Average users per guild:__**', f'**`{round(divided_users, 2)}`**')
+        embed.field('**__Invite me:__**', '**`n.invite`**')
+        embed.thumbnail('https://cdn.discordapp.com/avatars/713352863153258556/47823ecf46a380f770769b7a4a7c3449.png?size=256')
+        return await embed.send(ctx)'''
        
-        embed=Embed('Server Number', f'Lacan NTSport is currently used in `{len(self.client.guilds)}` servers by `{comma_users}` users. \nThis is an average of `{round(divided_users, 2)}` users per server.\nIn order to invite me to your server, use `n.invite.`', '1234')
-        embed.thumbnail('https://media.discordapp.net/attachments/719414661686099993/799587106673655838/Official_Lacan_NTSport_Logo.png?width=495&height=493')
+        embed=Embed('Server Number', f'**__Guilds:__ `{len(self.client.guilds)}`**\n\n**__Total users:__ `{comma_users}`**\n\n**__Average users per guild:__ `{round(divided_users, 2)}`**\n\n**__Invite me:__ `n.invite`**', '1234')
+        embed.thumbnail('https://cdn.discordapp.com/avatars/713352863153258556/47823ecf46a380f770769b7a4a7c3449.png?size=256')
         return await embed.send(ctx)
-        #else:
-            #await ctx.send(f'Lol. That\'s private! We\'re not gonna tell you the servers the bot is in.')
+        '''embed=Embed('Server Number', f'Lacan NTSport is currently used in `{len(self.client.guilds)}` servers by `{comma_users}` users. \nThis is an average of `{round(divided_users, 2)}` users per server.\nIn order to invite me to your server, use `n.invite.`', '1234')
+        embed.thumbnail('https://media.discordapp.net/attachments/719414661686099993/799587106673655838/Official_Lacan_NTSport_Logo.png?width=495&height=493')
+        return await embed.send(ctx)'''
 
 def setup(client):
     client.add_cog(Command(client))
