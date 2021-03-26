7fae6f9b26c25053589989ead4b8d376b3478001
Try2Win4Glory <65898600+Try2Win4Glory@users.noreply.github.com>
2021-03-26 08:52:05 +0100

    Update servernum.py
    Users, not Average users

--- commands/useful/servernum.py
+++ commands/useful/servernum.py
@@ -39,7 +39,7 @@ class Command(commands.Cog):
         embed.thumbnail('https://cdn.discordapp.com/avatars/713352863153258556/47823ecf46a380f770769b7a4a7c3449.png?size=256')
         return await embed.send(ctx)'''
        
-        embed=Embed('Server Number', f'**__Guilds:__ `{len(self.client.guilds)}`**\n\n**__Total users:__ `{comma_users}`**\n\n**__Average users per guild:__ `{round(divided_users, 2)}`**\n\n**__Invite me:__ `n.invite`**', '1234')
+        embed=Embed('Server Number', f'**__Guilds:__ `{len(self.client.guilds)}`**\n\n**__Total users:__ `{comma_users}`**\n\n**__Users per guild:__ `{round(divided_users, 2)}`**\n\n**__Invite me:__ `n.invite`**', '1234')
         embed.thumbnail('https://cdn.discordapp.com/avatars/713352863153258556/47823ecf46a380f770769b7a4a7c3449.png?size=256')
         return await embed.send(ctx)
         '''embed=Embed('Server Number', f'Lacan NTSport is currently used in `{len(self.client.guilds)}` servers by `{comma_users}` users. \nThis is an average of `{round(divided_users, 2)}` users per server.\nIn order to invite me to your server, use `n.invite.`', '1234')
