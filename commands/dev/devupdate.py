168f717395f7e1b8668f3d864e81a8703ce18ec9
‎ Try2Code4Glory <>
2021-04-25 11:11:50 +0000

    Team roles aren't globally anymore.

--- commands/dev/devupdate.py
+++ commands/dev/devupdate.py
@@ -40,7 +40,35 @@ class Command(commands.Cog):
             pass
         thelistofroles = ["Gold Member", [">99% Accuracy", "99% Accuracy", "98% Accuracy", "97% Accuracy", "96% Accuracy", "94-95% Accuracy", "90-93% Accuracy", "87-89% Accuracy", "84-86% Accuracy", "80-83% Accuracy", "75-79% Accuracy", "<75% Accuracy"], ["220+ WPM", "210-219 WPM", "200-209 WPM", "190-199 WPM", "180-189 WPM", "170-179 WPM", "160-169 WPM", "150-159 WPM", "140-149 WPM", "130-139 WPM", "120-129 WPM", "110-119 WPM", "100-109 WPM", "90-99 WPM", "80-89 WPM", "70-79 WPM", "60-69 WPM", "50-59 WPM", "40-49 WPM", "30-39 WPM", "20-29 WPM", "10-19 WPM", "1-9 WPM"], ["500000+ Races", "250000-499999 Races", "200000-249999 Races", "150000-199999 Races", "100000-149999 Races", "75000-99999 Races", "50000-74999 Races", "40000-49999 Races", "30000-39999 Races", "20000-29999 Races", "10000-19999 Races", "5000-9999 Races", "3000-4999 Races", "1000-2999 Races","500-999 Races", "100-499 Races", "50-99 Races", "1-49 Races"]]
         listofroles = ["Gold Member", ">99% Accuracy", "99% Accuracy", "98% Accuracy", "97% Accuracy", "96% Accuracy", "94-95% Accuracy", "90-93% Accuracy", "87-89% Accuracy", "84-86% Accuracy", "80-83% Accuracy", "75-79% Accuracy", "<75% Accuracy", "220+ WPM", "210-219 WPM", "200-209 WPM", "190-199 WPM", "180-189 WPM", "170-179 WPM", "160-169 WPM", "150-159 WPM", "140-149 WPM", "130-139 WPM", "120-129 WPM", "110-119 WPM", "100-109 WPM", "90-99 WPM", "80-89 WPM", "70-79 WPM", "60-69 WPM", "50-59 WPM", "40-49 WPM", "30-39 WPM", "20-29 WPM", "10-19 WPM", "1-9 WPM", "500000+ Races", "250000-499999 Races", "200000-249999 Races", "150000-199999 Races", "100000-149999 Races", "75000-99999 Races", "50000-74999 Races", "40000-49999 Races", "30000-39999 Races", "20000-29999 Races", "10000-19999 Races", "5000-9999 Races", "3000-4999 Races", "1000-2999 Races","500-999 Races", "100-499 Races", "50-99 Races", "1-49 Races"]
-        teamswithroles=['[DRPT]', '[TEST]', '[N8TE]', '[RRN]', '[NEWS]']
+        teamswithroles = [
+          # Insert Global Team Tags Here
+        ]
+
+        #Team N8TE | Server Owner: 630761745140547625
+        if ctx.guild.id in [
+          636582509429260289
+        ]:
+          teamswithroles.append('[N8TE]')
+        #Team DRPT | Server Owner: 723224207651111003
+        if ctx.cuild.id in [
+          742854336618561608
+        ]:
+          teamswithroles.append('[DRPT]')
+        #Team RRN | Server Owner: 653772108815532053
+        if ctx.guild.id in [
+          696055942055198760
+        ]:
+          teamswithroles.append('[RRN]')
+        #Team NEWS | Server Owner: 272370019894165505
+        if ctx.guild.id in [
+          835305919679692850
+        ]:
+          teamswithroles.append('[NEWS]')
+        #Team TEST | Server Owner: 505338178287173642
+        if ctx.guild.id in [
+          833317505888026644
+        ]:
+          teamswithroles.append('[TEST]')
         #data = requests.get('https://test-db.nitrotypers.repl.co', data={"key": os.getenv('DB_KEY')}).text
         #data = json.loads(data)
         dbclient = DBClient()
