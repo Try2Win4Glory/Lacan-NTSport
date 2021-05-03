e326c3660a22b991854535e33fa8d5ab190cba66
adl212 <>
2021-05-03 19:05:53 +0000

    get rid of debugger

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ async def on_message(message):
                 #print(f"{message.content} | {message.author.id} | {str(message.author)} | {message.guild.id} | {str(message.guild)}")
                 async with message.channel.typing():
                     await asyncio.sleep(random.uniform(0.05, 0.1))
-                return await client.process_commands(message)
+                #return await client.process_commands(message)
                 try:
                     ctx = await client.get_context(message)
                     await ctx.command.invoke(ctx)
