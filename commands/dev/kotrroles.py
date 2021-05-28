f125dcadf230e2f64fe7317782c6ae1d659463b6
Try2Win4Glory <65898600+Try2Win4Glory@users.noreply.github.com>
2021-05-28 16:25:11 +0200

    Update and rename roles3.py to kotrroles.py

--- commands/dev/kotrroles.py
+++ commands/dev/kotrroles.py
@@ -11,14 +11,14 @@ class Command(commands.Cog):
         self.client = client
     
     @commands.command()
-    async def roles3(self, ctx):
+    async def kotrroles(self, ctx):
         #return await ctx.send('This command is currently under maintenance. The developers will try to get it up again as soon as possible. In the meantime feel free to use `n.help` to get the other commands. Thank you for your understanding!')
         if (ctx.author.id) not in [396075607420567552, 505338178287173642, 637638904513691658]:
             embed = Embed('Error!', 'You\'re not a developer of this bot! Click [here](https://www.latlmes.com/entertainment/dev-application-1) to apply for dev.', 'warning')
 
             embed.footer(f'⚙️This command is a 🛠️developer🛠️ only command.⚙️', 'https://cdn.discordapp.com/attachments/719414661686099993/754971786231283712/season-callout-badge.png')
             return await embed.send(ctx)
-        embed = Embed('Success!', f'The roles for `{str(ctx.author.guild.id)}` were created by '+str(ctx.author.name + '#' + ctx.author.discriminator)+'.','white_check_mark')
+        embed = Embed('Success!', f'The roles for `{str(ctx.author.guild.id)}` were created by '+str(ctx.author.name + '#' + ctx.author.discriminator)+'.\n\n**Type:** King of the Race roles','white_check_mark')
         embed.footer(f'Discord user '+str(ctx.author.name + '#' + ctx.author.discriminator)+' is a 🛠️developer🛠️ of this bot.\n⚙️This command is a 🛠️developer🛠️ only command.⚙️', 'https://media.discordapp.net/attachments/719414661686099993/765490220858081280/output-onlinepngtools_32.png')
         guild = ctx.guild
         await embed.send(ctx)
