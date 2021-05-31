23bcd486b2c5bb6485f923432446f38969c6b2cb
Try2Win4Glory <65898600+Try2Win4Glory@users.noreply.github.com>
2021-05-31 05:03:00 +0200

    Add TBZ Team tag

--- commands/nitrotype/unregister.py
+++ commands/nitrotype/unregister.py
@@ -46,6 +46,11 @@ class Command(commands.Cog):
           833317505888026644
         ]:
           teamswithroles.append('[TEST]')
+        #Team TBZ | Server Owner: 657296213087092756
+        if ctx.guild.id in [
+            776221395725189140
+        ]:
+            teamswithroles.append('[TBZ]')
         premiumserver = False
         #dbdata = json.loads(requests.get('https://test-db.nitrotypers.repl.co', data={"key": dbkey}).text)
         dbclient = DBClient()
