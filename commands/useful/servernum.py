e09c04c57677effc682b0c166fd7c29147dc1d81
Try2Win4Glory <65898600+Try2Win4Glory@users.noreply.github.com>
2021-06-01 14:25:54 +0200

    Add Registered users  counter

--- commands/useful/servernum.py
+++ commands/useful/servernum.py
@@ -37,6 +37,11 @@ class Command(commands.Cog):
 
         guilds = len(self.client.guilds)
         divided_users = totalusers/guilds
+        
+        dbclient = DBClient()
+        collection = dbclient.db.nitrotype
+        registeredusers = await collection.count_documents({})
+        registered_users = "{:,}".format(registeredusers)
 
         '''embed=Embed(':1234:  Server Number', f'Check in how many servers I\'m used in!')
         embed.field('**__Guilds:__**', f'**`{len(self.client.guilds)}`**')
@@ -46,7 +51,7 @@ class Command(commands.Cog):
         embed.thumbnail('https://cdn.discordapp.com/avatars/713352863153258556/47823ecf46a380f770769b7a4a7c3449.png?size=256')
         return await embed.send(ctx)'''
        
-        embed=Embed('Server Number', f'**__Guilds:__ `{len(self.client.guilds)}`**\n\n**__Premium guilds:__ **`{premnum} ({round(prempercentage,3)}%)`\n\n**__Total users:__ `{comma_users}`**\n\n**__Users per guild:__ `{round(divided_users, 2)}`**\n\n**__Invite me:__ `n.invite`**', '1234')
+        embed=Embed('Server Number', f'**__Guilds:__ `{len(self.client.guilds)}`**\n\n**__Premium guilds:__ **`{premnum} ({round(prempercentage,2)}%)`\n\n**__Total users:__ `{comma_users}`**\n\n**__Registered users:__ `{registered_users}`**\n\n**__Users per guild:__ `{round(divided_users, 2)}`**\n\n**__Invite me:__ `n.invite`**', '1234')
         embed.thumbnail('https://cdn.discordapp.com/avatars/713352863153258556/47823ecf46a380f770769b7a4a7c3449.png?size=256')
         return await embed.send(ctx)
         '''embed=Embed('Server Number', f'Lacan NTSport is currently used in `{len(self.client.guilds)}` servers by `{comma_users}` users. \nThis is an average of `{round(divided_users, 2)}` users per server.\nIn order to invite me to your server, use `n.invite.`', '1234')
