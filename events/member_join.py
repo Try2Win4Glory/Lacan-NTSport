a13d3ec882dec2ba567003ce443f04cb60763571
Try2Win4Glory <65898600+Try2Win4Glory@users.noreply.github.com>
2021-06-19 05:20:30 +0200

    Update member_join.py

--- events/member_join.py
+++ events/member_join.py
@@ -36,7 +36,7 @@ class Events(commands.Cog):
         message = message.replace('{{user.racer.speed}}', speed)
         message = message.replace('{{user.racer.accuracy}}', accuracy)
         message = message.replace('{{user.racer.races}}', races)
-        embed=Embed(f'Welcome to {server.name}! :wave:', message)
+        embed=Embed(f'Welcome to {server}! :wave:', message)
         await channel.send(embed=Embed)
 def setup(client):
     client.add_cog(Events(client))
