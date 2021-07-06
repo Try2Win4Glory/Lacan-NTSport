b4a1af0ca76199a45d8d6f5f92e75d3173d7c654
unknown <>
2021-07-06 16:17:00 -0700

    fix stats on non verified people

--- commands/nitrotype/stats.py
+++ commands/nitrotype/stats.py
@@ -24,7 +24,7 @@ class Command(commands.Cog):
             except:
               embed=Embed('Error!', 'Couldn\'t find that user', 'warning')
         else:
-            racer = await get_username(user, True)
+            racer = await get_username(user)
         try:
             if racer[0] == True:
                 racer = racer[1]
