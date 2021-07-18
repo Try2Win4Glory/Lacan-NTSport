b7cb1d974a8f3c7c573452fedd57f9725ac2d155
Try2Win4Glory <65898600+Try2Win4Glory@users.noreply.github.com>
2021-07-18 12:34:26 +0200

    Update hang.py

--- commands/Economy/hang.py
+++ commands/Economy/hang.py
@@ -11,80 +11,75 @@ class Command(commands.Cog):
     
     @commands.command(aliases = ['hang', 'hm'])
     async def hangman(self, ctx):
-            print(f"{ctx.guild.name} - #{ctx.channel.name} - {ctx.author.name} - {ctx.message.content}")
-            with open('./commands/Economy/hw.txt') as f:
-                word = random.choice(f.readlines()).rstrip("\n")
-            hang = [
-                "**```    ____",
-                "   |    |",
-                "   |    ",
-                "   |   ",
-                "   |    ",
-                "   |   ",
-                "___|__________```**"
-            ]
-            empty = '\n'.join(hang)
-            man = [['@', 2], [' |', 3], ['\\', 3, 7], ['/', 3], ['|', 4], ['/', 5], [' \\', 5]]
-            string = [':blue_square:' for i in word]
-            embed=Embed('Nitro Type Hangman', f'Type a letter in the chat to guess.\n\n**{" ".join(string)}**\n\n{empty}')
-            #original = await embed.send(ctx)
-            '''embed = discord.Embed(
-                title = "Hangman",
-                color = ctx.author.color,
-                description = f"Type a letter in chat to guess.\n\n**{' '.join(string)}**\n\n{empty}",
-            )'''
-            incorrect = 0
-            original = await ctx.send(embed = embed)
-            guessed = []
-            incorrect_guessed = []
-            already_guessed = None
-            def check(m):
-                return m.channel == ctx.channel and m.content.isalpha() and len(m.content) == 1 and m.author == ctx.author
-            while incorrect < len(man) and ':blue_square:' in string:
-                try:
-                    msg = await self.client.wait_for('message', timeout = 120.0, check = check)
-                    letter = msg.content.lower()
-                except asyncio.TimeoutError:
-                    await ctx.send("Your Game timed out.")
-                    return
-                if already_guessed:
-                    await already_guessed.delete()
-                    already_guessed = None
-                if letter in guessed:
-                    already_guessed = await ctx.send("You have already guessed that letter.")
-                    await msg.delete()
-                    continue
-                guessed += letter
-                if letter not in word:
-                    incorrect_guessed += letter
-                    '''if embed.fields:
-                        #embed.field_at(0, name = "Incorrect letters:", value = ', '.join(incorrect_guessed))
-                        
-                    else:
-                        #embed.field(name = "Incorrect letters:", value = ', '.join(incorrect_guessed))'''
-                    embed.field('Incorrect letters:', f'", ".join{incorrect_guessed}')
-                    
-                    part = man[incorrect]
-                    if len(part) > 2:
-                        hang[part[1]] = hang[part[1]][0:part[2]] + part[0] + hang[part[1]][part[2] + 1:]
-                    else:
-                        hang[part[1]] += part[0]
-                    incorrect += 1
-                else:
-                    for j in range(len(word)):
-                        if letter  == word[j]:
-                            string[j] = word[j]
-                new = '\n'.join(hang)
-                if ':blue_square:' not in string:
-                    embed.description = f'You guessed the word!\n\n**{" ".join(string)}**\n\n{new}'
-                elif incorrect == len(man):
-                    embed.description = f'You\'ve been hanged! The word was \n\n**{" ".join([k for k in word])}**\n\n{new}'
-                else:
-                    embed.description = f'Type a letter in chat to guess.\n\n**{" ".join(string)}**\n\n{new}'
+        print(f"{ctx.guild.name} - #{ctx.channel.name} - {ctx.author.name} - {ctx.message.content}")
+        with open('./commands/Economy/hw.txt') as f:
+            word = random.choice(f.readlines()).rstrip("\n")
+        hang = [
+            "**```    ____",
+            "   |    |",
+            "   |    ",
+            "   |   ",
+            "   |    ",
+            "   |   ",
+            "___|__________```**"
+        ]
+        empty = '\n'.join(hang)
+        man = [['@', 2], [' |', 3], ['\\', 3, 7], ['/', 3], ['|', 4], ['/', 5], [' \\', 5]]
+        string = [':blue_square:' for i in word]
+        embed = discord.Embed(
+            title = "Hangman",
+            color = ctx.author.color,
+            description = f"Type a letter in chat to guess.\n\n**{' '.join(string)}**\n\n{empty}",
+        )
+        incorrect = 0
+        original = await ctx.send(embed = embed)
+        print('File Hang.py: \'original\' success!')
+        guessed = []
+        incorrect_guessed = []
+        already_guessed = None
+        def check(m):
+            return m.channel == ctx.channel and m.content.isalpha() and len(m.content) == 1 and m.author == ctx.author
+        while incorrect < len(man) and ':blue_square:' in string:
+            try:
+                msg = await self.client.wait_for('message', timeout = 120.0, check = check)
+                letter = msg.content.lower()
+            except asyncio.TimeoutError:
+                await ctx.send("Your Game timed out.")
+                return
+            if already_guessed:
+                await already_guessed.delete()
+                already_guessed = None
+            if letter in guessed:
+                already_guessed = await ctx.send("You have already guessed that letter.")
                 await msg.delete()
-                #await original.edit(embed = embed)
-                await original.edit(embed=embed)
-
+                continue
+            guessed += letter
+            if letter not in word:
+                incorrect_guessed += letter
+                if embed.fields:
+                    embed.set_field_at(0, name = "Incorrect letters:", value = ', '.join(incorrect_guessed))
+                else:
+                    embed.add_field(name = "Incorrect letters:", value = ', '.join(incorrect_guessed))
+                part = man[incorrect]
+                if len(part) > 2:
+                    hang[part[1]] = hang[part[1]][0:part[2]] + part[0] + hang[part[1]][part[2] + 1:]
+                else:
+                    hang[part[1]] += part[0]
+                incorrect += 1
+            else:
+                for j in range(len(word)):
+                    if letter  == word[j]:
+                        string[j] = word[j]
+            new = '\n'.join(hang)
+            if ':blue_square:' not in string:
+                earnings = random.randint(5, 30)
+                embed.description = f"You guessed the word!\n\n**{' '.join(string)}**\n\n{new}"
+            elif incorrect == len(man):
+                embed.description = f"You've been hanged! The word was \n\n**{' '.join([k for k in word])}**\n\n{new}"
+            else:
+                embed.description = f"Type a letter in chat to guess.\n\n**{' '.join(string)}**\n\n{new}"
+            await msg.delete()
+            await original.edit(embed = embed)
     '''@hangman.error
     async def hangman_error(self, ctx, error):
         await ctx.send(error)'''    
