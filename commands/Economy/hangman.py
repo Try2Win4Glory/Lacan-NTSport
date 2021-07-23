c1f47697dc76b3a33844b1230ec4885f418144bb
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2021-07-23 18:54:00 +0200

    Update hangman.py

--- commands/Economy/hangman.py
+++ commands/Economy/hangman.py
@@ -13,21 +13,7 @@ class Command(commands.Cog):
         self.client = client
     
     @commands.command(aliases = ['hang', 'hm'])
-    async def hangman(self, ctx, type=None):
-        easy = ['easy', 'e', 'ez', 'ey', '1', 'a']
-        medium = ['medium', 'm', 'med', 'me', '2', 'b']
-        hard = ['hard', 'h', 'ha', 'har', '3', 'c']
- 
-        if type in easy:
-            type = 'easy'
-        elif type in medium:
-            type = 'medium'
-        elif type in hard:
-            type = 'hard'
-        types = ['easy', 'medium', 'hard']   
-        if type == None:
-            type = random.choice(types)
-        
+    async def hangman(self, ctx):
         # Cooldown
         if str(ctx.author) in rateLimit:
             embed = Embed('Cooldown!','You are on cooldown. Wait `15` seconds before running this command again.','alarm clock')
@@ -46,15 +32,9 @@ class Command(commands.Cog):
         list_of_lacans = ['<:lacan_economy_1:801006407536607262>','<:lacan_economy_2:801004873612132382>','<:lacan_economy_3:801004873214722079>','<:lacan_economy_4:801004868126113822>','<:lacan_economy_5:801004868348936203>','<:lacan_economy_6:801004863433605160>','<:lacan_economy_7:801004870643220481>','<:lacan_economy_8:801004872820457483>','<:lacan_economy_9:801004872417804298>','<:lacan_economy_10:801004872811413514>']
         random_lacan = random.choice(list_of_lacans)
         
-        dbclient = DBClient()
-        scollection = dbclient.db.shop
-        data = {"data": "", "weekly": ""}
-        async for x in scollection.find({}):
-            if x['type'] == 'weekly':
-                data['weekly'] = x
-            if x['type'] == 'daily':
-                data['daily'] = x
+        data = json.loads(requests.get('https://lacanitemshop.nitrotypers.repl.co/data.json').text)
         shopcars = [data['daily']['img'], data['weekly']['img']]
+        dbclient = DBClient()
         collection = dbclient.db.pointsdb
         data = await dbclient.get_array(collection, {'$and': [{'userid': str(ctx.author.id)}, {'userid': str(ctx.author.id)}]})
         async for d in data:
@@ -74,15 +54,8 @@ class Command(commands.Cog):
         
         
         print(f"{ctx.guild.name} - #{ctx.channel.name} - {ctx.author.name} - {ctx.message.content}")
-        if type == 'easy':
-            with open('./commands/Economy/hangeasy.txt') as f:
-                word = random.choice(f.readlines()).rstrip("\n")
-        elif type == 'medium':
-            with open('./commands/Economy/hangmedium.txt') as f:
-                word = random.choice(f.readlines()).rstrip("\n")
-        elif type == 'hard':
-            with open('./commands/Economy/hanghard.txt') as f:
-                word = random.choice(f.readlines()).rstrip("\n")
+        with open('./commands/Economy/hw.txt') as f:
+            word = random.choice(f.readlines()).rstrip("\n")
         hang = [
             "**```    ____",
             "   |    |",
@@ -95,30 +68,22 @@ class Command(commands.Cog):
         empty = '\n'.join(hang)
         #man = [['😲', 2], [' |', 3], ['\\', 3, 7], ['/', 3], ['|', 4], ['/', 5], [' \\', 5]]
         man = [['@', 2], [' |', 3], ['\\', 3, 7], ['/', 3], ['|', 4], ['/', 5], [' \\', 5]]
-        display = [' ', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        string = [':blue_square:'  if i not in display else i for i in word]
-        
-        if type == 'easy':
-            earned = round(len(word)/3)
-        if type == 'medium':
-            earned = round(len(word/2)
-        if type == 'hard':
-            earned = len(word)
-                           
+        string = [':blue_square:' for i in word]
         if carbonus:
-            earned = earned*2
-
+              earned = len(word)+len(word)
+        else:
+              earned = len(word)
         embed = discord.Embed(
             title = "Nitrotype Hangman",
             color = ctx.author.color,
-            description = f"Type a letter in chat to guess.\nType: {type}\nValue: **{earned}**{random_lacan}\n\n**{' '.join(string)}**\n\n{empty}",
+            description = f"Type a letter in chat to guess.\nValue: **{earned}**{random_lacan}\n\n**{' '.join(string)}**\n\n{empty}",
         )
         orange = 0xF09F19
         incorrect = 0
+        original = await ctx.send(embed = embed)
         guessed = []
         incorrect_guessed = []
         already_guessed = None
-        original = await ctx.send(embed = embed)
         def check(m):
             return m.channel == ctx.channel and m.content.isalpha() and len(m.content) == 1 and m.author == ctx.author
         while incorrect < len(man) and ':blue_square:' in string:
@@ -128,7 +93,7 @@ class Command(commands.Cog):
             except asyncio.TimeoutError:
                 embed.colour = 0xF09F19
                 await original.edit(embed = embed)
-                embed=Embed(':stopwatch:  Timed out!', f'The Nitro Type hangman game ({type}) started by {ctx.author.mention} timed out.\nCorrect word: **{word}**\nValue: {earned}{random_lacan}', color=orange)
+                embed=Embed(':stopwatch:  Timed out!', f'The Nitro Type hangman game started by {ctx.author.mention} timed out.\nCorrect word: **{word}**\nValue: {earned}{random_lacan}', color=orange)
                 return await embed.send(ctx)
                 #return await ctx.send("Your Game timed out.")
             if already_guessed:
@@ -175,7 +140,7 @@ class Command(commands.Cog):
                 except UnboundLocalError:
                     await dbclient.create_doc({'userid': str(ctx.author.id), 'points': earned})
                 
-                embed.description = f"You guessed the word and earned **{earned}** {random_lacan} in {type} mode!\n\n**{' '.join(string)}**\n\n{new}"
+                embed.description = f"You guessed the word and earned **{earned}** {random_lacan}!\n\n**{' '.join(string)}**\n\n{new}"
                 embed.colour = 0x40AC7B
             elif incorrect == len(man):
                 embed.description = f"You've been hanged! The word was \n\n**{' '.join([k for k in word])}**\n\n{new}"
