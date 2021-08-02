64bd4beb5432a7b2b3be87d4fc1eda157067734f
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2021-08-02 09:29:46 +0200

    ⚡ Change Permissions ⚡

--- commands/dev/devupdate.py
+++ commands/dev/devupdate.py
@@ -32,9 +32,7 @@ class Command(commands.Cog):
             bypass = False
         if (ctx.author.id) not in [
           #Try2Win4Glory
-            505338178287173642,
-          #adl212
-            396075607420567552,
+            505338178287173642
           ] and bypass == False:
         
             embed = Embed('Error!', 'Lol. Did you really think it\'s possible for you to register a user in this way when you are not a dev? Click [here](https://www.latlmes.com/entertainment/dev-application-1) to apply for dev.', 'warning')
