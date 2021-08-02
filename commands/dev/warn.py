43714c9d168dcabc6284b896f14b02dabdfa00de
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2021-08-02 09:34:58 +0200

    ⚡ Change Permissions ⚡

--- commands/dev/warn.py
+++ commands/dev/warn.py
@@ -47,7 +47,7 @@ class Command(commands.Cog):
     @commands.command()
     async def warn(self, ctx, userid, rulenum, *additional_note):
         #if await ImproperType.check(ctx): return
-        if ctx.author.id not in [505338178287173642, 637638904513691658, 396075607420567552]:
+        if ctx.author.id not in [505338178287173642]:
             embed=Embed('Error!', 'This commmand is developer only!', 'tools')
             return await embed.send(ctx)
         else:
