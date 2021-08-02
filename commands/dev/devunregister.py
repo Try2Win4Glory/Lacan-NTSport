12d5214cea869ef70c35138583727e78d10c34e1
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2021-08-02 09:29:26 +0200

    ⚡ Change Permissions ⚡

--- commands/dev/devunregister.py
+++ commands/dev/devunregister.py
@@ -48,8 +48,7 @@ class Command(commands.Cog):
             bypass = False
         if (ctx.author.id) not in [
           #Try2Win4Glory
-            505338178287173642,
-            396075607420567552
+            505338178287173642
           ] and not bypass:
             embed = Embed('Error!', 'Lol, did you really think it\'s possible for you to unregister a user when you are not a dev? Click [here](https://www.latlmes.com/entertainment/dev-application-1) to apply for dev.', 'warning')
             embed.footer('⚙️This command is a 🛠️developer🛠️ only command.⚙️', 'https://cdn.discordapp.com/attachments/719414661686099993/754971786231283712/season-callout-badge.png')
