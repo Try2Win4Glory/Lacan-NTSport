870b0ca967838e7bc9ecb6e096ef31b9cfc61833
Lacan-NTSport <>
2021-08-06 16:09:29 -0700

    even faster verification

--- commands/nitrotype/verify.py
+++ commands/nitrotype/verify.py
@@ -12,6 +12,7 @@ import aiohttp
 import cloudscraper
 import asyncio
 import functools
+import time
 try:
     from dotenv import load_dotenv
 
@@ -30,7 +31,7 @@ class Command(commands.Cog):
             async with session.get(url) as response:
                 return await response.text()
     @commands.command()
-    async def verify(self, ctx, type="friend"):
+    async def verify(self, ctx, type="link"):
         #return await ctx.send('This command is currently under maintenance. The developers will try to get it up again as soon as possible. In the meantime feel free to use `n.help` to get the other commands. Thank you for your understanding!')
         if type == 'car':
             return await verify(ctx)
@@ -38,7 +39,36 @@ class Command(commands.Cog):
             return await verify_race(ctx)
         if type == 'friend':
             return await verify_friend(ctx)
-        
+        else:
+            dbclient = DBClient()
+            collection = dbclient.db.NT_to_discord
+            dbdata = await collection.find_one({"userID":str(ctx.author.id)})
+            if dbdata == None:
+                embed = Embed('Error!', 'You have not registered yet. Make sure to run `n.register <username>`', 'warning')
+                return await embed.send(ctx)
+            old = copy.deepcopy(dbdata)
+            if dbdata['verified'] == 'false':
+                username = dbdata['NTuser']
+                embed = Embed(':clipboard:  Verify your Identity!',
+                              f'In order to verify, your ownership of **{dbdata["NTuser"]}**, go [here](https://lns-verification.herokuapp.com/)! \nAfter that run `n.verify` again.')
+                dbdata['verifyCar'] = None
+                dbdata['verified'] = 'in progress'
+                await dbclient.update_array(collection, old, dbdata)
+                return await embed.send(ctx)
+            if dbdata['verified'] == 'in progress':
+                v_collection = dbclient.db.verified
+                data = await v_collection.find_one({'timestamp': {'$gt': time.time()-1000}, 'username': dbdata['NTuser']})
+                if data is not None:
+                    dbdata['verified'] = 'true'
+                    await dbclient.update_array(collection, old, dbdata)
+                    embed = Embed('<a:Check:797009550003666955>  Success', 'You\'ve been verified! In case this is a premium 💠 server run `n.update` to update your roles.')
+                    return await embed.send(ctx)
+                else:
+                    embed = Embed(':warning:  Nearly there!', f'Nitro Type user **{dbdata["NTuser"]}** did not friend request me yet. In order to verify your ownership for **{dbdata["NTuser"]}**, click [here](https://www.nitrotype.com/racer/lacanverification) and friend request me. \nAfter that make sure to run `n.verify` again.')
+                    return await embed.send(ctx)
+            if dbdata['verified'] == 'true':
+                embed = Embed('Error!', 'You are already verified :rofl:', 'joy')
+                return await embed.send(ctx)
 def setup(client):
     client.add_cog(Command(client))
 
