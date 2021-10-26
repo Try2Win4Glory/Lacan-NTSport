4e3dc6b72d8917ae2d8327e447d69fb5d7fa44f8
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2021-10-26 10:29:34 +0200

    ⚡ Add new Role Ideas ⚡

--- commands/Premium/update.py
+++ commands/Premium/update.py
@@ -300,26 +300,30 @@ class Command(commands.Cog):
             try:
                 if int(racer.created_timestamp) <= 1430172000:
                         role = get(ctx.message.guild.roles, name="v1 Veteran")
-                        await ctx.author.add_roles(role)
+                        if role != 'None':
+                            roles_to_add.append(role)
             except:
                 pass
             try:
                 if int(racer.created_timestamp) > 1430172000 and racer.created_timestamp <= 1559685600:
                         role = get(ctx.message.guild.roles, name="v2 Veteran")
-                        await ctx.author.add_roles(role)
+                        if role != 'None':
+                            roles_to_add.append(role)
             except:
                 pass
             try:
                 if int(racer.longest_session_sessionist) >= 800:
                         role = get(ctx.message.guild.roles, name="Sessionist")
-                        await ctx.author.add_roles(role)
+                        if role != 'None':
+                            roles_to_add.append(role)
             except:
                 pass
 
             try:
                 if int(racer.popular_views) >= 10000:
                     role=get(ctx.message.guild.roles, name="Popular")
-                    await ctx.author.add_roles(role)
+                    if role != 'None':
+                        roles_to_add.append(role)
             except:
                 pass
                     
@@ -334,17 +338,40 @@ class Command(commands.Cog):
             if gold == 'lifetime':
                 try:
                     role = get(ctx.message.guild.roles, name="Lifetime Gold")
-                    await ctx.author.add_roles(role)
+                    if role != 'None':
+                        roles_to_add.append(role)
                 except:
                     print('Update: Failed to add Lifetime Gold role.')
             elif gold == 'yearly':
                 try:
                     role = get(ctx.message.guild.roles, name="Yearly Gold")
-                    await ctx.author.add_roles(role)
+                    if role != 'None':
+                        roles_to_add.append(role)
                 except:
                     print('Update: Failed to add Yearly Gold role.')
             else:
                 pass
+            try:
+                if int(racer.nitros_used) >= 10000:
+                    role = get(ctx.message.guild.roles, name="Nitro Enthusiast")
+                    if role != 'None':
+                        roles_to_add.append(role)
+            except:
+                pass
+            try:
+                if int(racer.car_collector) >= 200:
+                    role = get(ctx.message.guild.roles, name="Car Collector")
+                    if role != 'None':
+                        roles_to_add.append(role)
+            except:
+                pass
+            try:
+                if int(racer.high_speed)-int(racer.average_speed) >= 50:
+                    role = get(ctx.message.guild.roles, name="Undulation Master")
+                    if role != 'None':
+                        roles_to_add.append(role)
+            except:
+                    pass
                     
             print(roles_to_add)
 
