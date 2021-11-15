bd1cf590df236cdf8569cf97b70956f07cf592d3
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2021-11-15 14:14:54 +0100

    Update nitrotype.py

--- packages/nitrotype.py
+++ packages/nitrotype.py
@@ -1079,15 +1079,15 @@ class RacerClass:
 		#self.lifetime_gold_role = None
 		#self.yearly_gold_role = None
             else:
-		try:
+                try:
                     get(ctx.message.guild.roles, name='Gold Member')
                     self.gold_role = 'Gold Member'
-		except:
+                except:
                     self.gold_role = 'Gold Member'
                     if self.lifetime_gold == True:
-			self.gold_role = 'Lifetime Gold'
+                        self.gold_role = 'Lifetime Gold'
                     else:
-			self.gold_role = 'Yearly Gold'
+                        self.gold_role = 'Yearly Gold'
             self.friend_reqs_allowed = ':white_check_mark: ' if newdata['allowFriendRequests'] == 1 else ':negative_squared_cross_mark:'
             self.looking_for_team = ':white_check_mark: ' if newdata['lookingForTeam'] == 1 else ':negative_squared_cross_mark:'
 async def Racer(username):
