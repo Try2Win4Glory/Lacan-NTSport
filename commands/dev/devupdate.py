9b096a0d4c556b22da4173445355da13786c52c8
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2022-01-30 14:14:08 +0100

    Update Permission System

--- commands/dev/devupdate.py
+++ commands/dev/devupdate.py
@@ -28,12 +28,12 @@ class Command(commands.Cog):
           return await embed.send(ctx)
 
       dbclient = DBClient()
-      collection = dbclient.client.dev.devupdate
+      permcollection = dbclient.client.dev.devupdate
       serversearch = ctx.guild.id
       print(serversearch)
-      x = await collection.find_one({"serverID":str(serversearch)})
+      x = await permcollection.find_one({"serverID":str(serversearch)})
       
-      dev = await collection.find_one({"bypass":"dev"})
+      dev = await permcollection.find_one({"bypass":"dev"})
       bypass = False
       if dev != None:
         if str(ctx.author.id) in str(dev["dev"]):
