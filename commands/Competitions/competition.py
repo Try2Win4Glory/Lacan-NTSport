ab523b1b37f7c45c32b7b68c1083daa2a963692d
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2022-02-01 14:20:46 +0100

    Fix Competitions

--- commands/Competitions/competition.py
+++ commands/Competitions/competition.py
@@ -10,26 +10,26 @@ class Command(commands.Cog):
     @commands.command(aliases=['comp', 'comps', 'teamcomp', 'teamcomps'])
     async def competition(self, ctx, team=None, thetime=None, type=None):
         if team == None:
-            return await ctx.send('Can you at least give me what team you want the competition for?')
+            return await ctx.send('Can you at least give me the Team you want the competition for? (Example: `n.competition SSH 1 h`)')
         if thetime == None:
-            return await ctx.send('You didn\'t specify the time left in the competiton.')
+            return await ctx.send('You didn\'t specify the time left in the competiton. (Example: `n.competition SSH 1 h`)')
         if type == None:
-            return await ctx.send('You didn\'t specify if the time was by months, days, hours, or minutes. Make sure to have a space. (Example: `n.competition SSH 1 h`.')
+            return await ctx.send('You didn\'t specify if the time was by months, days, hours, or minutes. Make sure to have a space. (Example: `n.competition SSH 1 h`).')
         endcomptime = round(time.time())
         if type == 'mo':
             endcomptime += 2592000*int(thetime)
-            await ctx.send(f'The competition will end in {thetime} months. Make sure to have your DMs turned on, otherwise the results of the competition will be lost.')
+            await ctx.send(f'The competition will end in {thetime} months. Check the Link down below for Accurate Live Results.')
         if type == 'd':
             endcomptime += 86400*int(thetime)
-            await ctx.send(f'The competition will end in {thetime} days. Make sure to have your DMs turned on, otherwise the results of the competition will be lost.')
+            await ctx.send(f'The competition will end in {thetime} days. Check the Link down below for Accurate Live Results.')
         if type == 'h':
             endcomptime += 3600*int(thetime)
-            await ctx.send(f'The competition will end in {thetime} hours. Make sure to have your DMs turned on, otherwise the results of the competition will be lost.')
+            await ctx.send(f'The competition will end in {thetime} hours. Check the Link down below for Accurate Live Results.')
         if type == 'm':
             endcomptime += 60*int(thetime)
-            await ctx.send(f'The competition will end in {thetime} minutes. Make sure to have your DMs turned on, otherwise the results of the competition will be lost.')
+            await ctx.send(f'The competition will end in {thetime} minutes. Check the Link down below for Accurate Live Results.')
         if type not in ['mo', 'd', 'h', 'm']:
-            await ctx.send('You can use mo for month, d for day, h for hour, m for minutes.')
+            await ctx.send('You can use `mo` for month, `d` for day, `h` for hour, `m` for minutes.')
             return
         f = open('usedids', 'a+')
         ids = f.readlines()
@@ -42,6 +42,7 @@ class Command(commands.Cog):
             else:
                 break
         f.write(compid+'\n')
+        team = team.upper()
         await nitrotype.create_comp(team, compid, endcomptime, ctx.author.id)
         embed = discord.Embed(
             title=':white_check_mark:  Competition Created!',
@@ -49,7 +50,7 @@ class Command(commands.Cog):
             color=0x15E700
         )
         embed.add_field(name="Competition ID", value=f'`{compid}`')
-        embed.set_footer(text="Don't lose your competition ID! You need it to check results and end the competition!")
+        embed.set_footer(text="Don't lose your competition ID! You need it to check results!")
         await ctx.send(embed=embed)
     
 def setup(client):
