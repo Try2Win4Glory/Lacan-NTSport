98fc79b6b317b8f16dd600ddabc6f0dcdcfc3f76
⚡Try2Win4Glory⚡ <65898600+Try2Win4Glory@users.noreply.github.com>
2022-02-02 17:50:25 +0100

    Rebrand Stats Command

--- commands/nitrotype/stats.py
+++ commands/nitrotype/stats.py
@@ -64,36 +64,89 @@ class Command(commands.Cog):
         '''if racer.car == 'https://www.nitrotype.com/cars/110_large_1.png':
             print('Minnie the coper alarm')
         else:'''
+        #embed.image(f'https://nitrotype.com{racer.trail_image}')
         embed.thumbnail(racer.car)
 
 
         #print(racer.name+' is from '+racer.country)
 
-        embed.field('__General Info__', f':small_blue_diamond: Level: **{racer.level}**\n:small_blue_diamond: XP: **{racer.experience}**\n:eyes: **{racer.views}** \n :birthday: **{racer.created}** \n <:nt_gold:861944319102418944> **{"Lifetime" if racer.lifetime_gold else "Gold" if racer.newdata["membership"] != "basic" else "Basic"}**', inline=True)
+        if racer.lifetime_gold:
+          gold = 'Lifetime'
+        elif racer.newdata["membership"] != "basic":
+          gold = 'Yearly'
+        else:
+          gold = 'Basic'
+        
+        header = '-------------------'
+        space = '       '
+        if racer.trailname != 'None':
+          trail_created = racer.newdata['loot'][0]['createdStamp']
+        else:
+          pass
+        
+        created = racer.newdata['createdStamp']
+        hammercreated = f'<t:{created}:f>'
+
+        races = racer.newdata['racesPlayed']
+        nitros = racer.newdata['nitrosUsed']
+        nitrosperrace = (races)/(nitros)
+        roundednitrosperrace = round(nitrosperrace, 2)
+
+        embed.field(f':race_car:{space}Racing', f'{header}:checkered_flag: **{racer.races}**\n:timer: **{racer.longest_session}**\n:stopwatch: Average: **{racer.wpm_average}**\n:stopwatch: High: **{racer.wpm_high}**')
+
+        embed.field(f'<:info:938430460227358771>{space}Useful', f'{header}:diamond_shape_with_a_dot_inside: Level **{racer.level}**\n:diamond_shape_with_a_dot_inside: **{racer.experience} XP**\n:eyes: **{racer.views}**\n:tada: {hammercreated}')
+
+        if gold == 'Lifetime':
+          goldemoji = '<:old_nt_gold:938434881933938699>'
+        elif gold == 'Yearly':
+          goldemoji = '<:nt_gold:861944319102418944>'
+        else:
+          goldemoji = '<:nt_basic:868772526321438740>'
+
+        if gold != 'Yearly':
+          embed.field(f':medal:{space}Gold Info', f'{header}{goldemoji} `{gold}`')
+        else:
+          embed.field(f':medal:{space}Gold Info', f'{header}{goldemoji} `{gold}`\n{goldemoji} Expires: <t:{racer.gold_until}:d>\n{goldemoji} Purchased: {racer.last_purchase}')
+          
+
+        embed.field(f':police_car:{space}Cars', f'{header}:blue_car: Owned: {racer.cars_owned}\n:blue_car: ID: `{racer.carid}`')
+
+        if racer.trailname != 'None':
+          embed.field(f':star: {space}Trails', f'{header}🌠 **{racer.trailname}**\n🌠 **{racer.trail_rarity}**\n🌠 <t:{trail_created}:d> <t:{trail_created}:t>')
+        else:
+          pass
+
+        embed.field(f'<:nitro:938450286488457327>{space}Nitros', f'{header}:zap: **∞** owned\n:zap: **{racer.nitros_used}** used\n:zap: **{roundednitrosperrace}**/:checkered_flag:')
+
+        embed.field(f':gear:{space}Add / Invite', f'{header}\n:bust_in_silhouette: Friends: {racer.friend_reqs_allowed}\n:bust_in_silhouette: Teams: {racer.looking_for_team}')
+
+
+
+        #embed.field('__General Info__', f':small_blue_diamond: Level: **{racer.level}**\n:small_blue_diamond: XP: **{racer.experience}**\n:eyes: **{racer.views}** \n :birthday: **{racer.created}** \n <:nt_gold:861944319102418944> **{"Lifetime" if racer.lifetime_gold else "Gold" if racer.newdata["membership"] != "basic" else "Basic"}**', inline=True)
 
         #Achievement points: **{racer.points}**
 
         #embed.field('\n\u200b', '\n\u200b')
 
-        embed.field('__Speed__', f':stopwatch: Avg: **{racer.wpm_average}**\n:stopwatch: High: **{racer.wpm_high}**')
+        #embed.field('__Speed__', f':stopwatch: Avg: **{racer.wpm_average}**\n:stopwatch: High: **{racer.wpm_high}**')
 
         #embed.field('\n\u200b', '\n\u200b')
 
-        embed.field('__Races__', f':checkered_flag: **{racer.races}**\n:timer: **{racer.longest_session}**', inline=True)
+        #embed.field('__Races__', f':checkered_flag: **{racer.races}**\n:timer: **{racer.longest_session}**', inline=True)
 
         #embed.field('\n\u200b', '\n\u200b')
 
-        embed.field('__Cars__', f':race_car: **{racer.cars_owned}** owned\n:race_car: **{racer.cars_sold}** sold\n:race_car: **{racer.cars_total}** total')
+        #embed.field('__Cars__', f':race_car: **{racer.cars_owned}** owned\n:race_car: **{racer.cars_sold}** sold\n:race_car: **{racer.cars_total}** total')
 
         #embed.field('\n\u200b', '\n\u200b')
         
-        if racer.trailname != 'None':
-            embed.field('__Trails__', f'🌠 Name: **{racer.trailname}**\n🌠 Asset Key: **{racer.trail_asset}**\n🌠 Rarity: **{racer.trail_rarity}**\n🌠 Created: **{racer.trail_created}**\n🌠 Image: [Link](https://nitrotype.com{racer.trail_image})')
-        else:
-            pass
+        #if racer.trailname != 'None':
+            #embed.field('__Trails__', f'🌠 Name: **{racer.trailname}**\n🌠 Asset Key: **{racer.trail_asset}**\n🌠 Rarity: **{racer.trail_rarity}**\n🌠 Created: **{racer.trail_created}**\n🌠 Image: [Link](https://nitrotype.com{racer.trail_image})')
+        #else:
+            #pass
         #embed.field('\n\u200b', '\n\u200b')
 
-        embed.field('__Nitros__', f':zap: **{racer.nitros}** owned\n:zap: **{racer.nitros_used}** used\n:zap: **{racer.nitros_total}** total')
+        #embed.field('__Nitros__', f':zap: **{racer.nitros}** owned\n:zap: **{racer.nitros_used}** used\n:zap: **{racer.nitros_total}** total')
 
         #embed.field('\u200b', '\u200b')
 
@@ -111,7 +164,7 @@ class Command(commands.Cog):
         except:
             pass
 
-        embed.field('__Settings__', f':gear: Friends: {racer.friend_reqs_allowed}\n:gear: Team invite: {racer.looking_for_team}')
+        #embed.field('__Settings__', f':gear: Friends: {racer.friend_reqs_allowed}\n:gear: Team invite: {racer.looking_for_team}')
 
 
         await embed.send(ctx)
