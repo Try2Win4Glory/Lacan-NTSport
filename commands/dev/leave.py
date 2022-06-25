ce829e88fb058aa2504b2cf15ba3b80d1f68067c
Malakai111 <77570597+Malakai111@users.noreply.github.com>
2022-06-25 00:20:05 -0500

    Update leave.py
    Signed-off-by: Malakai111 <77570597+Malakai111@users.noreply.github.com>

--- commands/dev/leave.py
+++ commands/dev/leave.py
@@ -12,7 +12,7 @@ class Command(commands.Cog):
     @commands.command()
     async def leave(self, ctx, guildid):
         #return await ctx.send('This command is currently under maintenance. The developers will try to get it up again as soon as possible. In the meantime feel free to use `n.help` to get the other commands. Thank you for your understanding!')
-        if ctx.author.id not in [505338178287173642]:
+        if ctx.author.id not in [505338178287173642, 724772394748870718]:
             return await ctx.send('bruh, you are not a developer')
         guild = get(self.client.guilds, id=int(guildid))
         if guild is None:
